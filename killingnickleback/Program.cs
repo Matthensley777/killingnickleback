13164efa9694b1954ef9e7bceafbe147f5907a35
developer <developer@DESKTOP-L75S7D1>
2018-01-22 18:01:20 -0600

    finished

--- killingnickleback/Program.cs
+++ killingnickleback/Program.cs
@@ -10,6 +10,32 @@ namespace killingnickleback
     {
         static void Main(string[] args)
         {
+            var goodSongs = new List<Song>();
+
+            var allSongs = new HashSet<Song>
+            {
+                new Song { Name = "Stairway to Heaven", Artist = "Led Zeplin"},
+                new Song { Name = "Photograph", Artist = "Nickleback"},
+                new Song { Name = "How You Remind Me", Artist = "Nickleback"},
+                new Song { Name = "The Wall", Artist = "Pink Floyd"},
+                new Song { Name = "Rockstar", Artist = "Nickleback"},
+                new Song { Name = "Hey Jude", Artist = "The Beatles"},
+                new Song { Name = "Hotel California", Artist = "Eagles"}
+            };
+            foreach( var song in allSongs)
+            {
+                if (song.Artist != "Nickleback")
+                {
+                    goodSongs.Add(song);
+                }
+                
+            }
+            foreach ( var song in goodSongs)
+            {
+                Console.WriteLine(song.Name);
+                
+            }
+            Console.ReadLine();
         }
     }
 }
